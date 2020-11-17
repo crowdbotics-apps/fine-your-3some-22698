fcfd898c96e9cd4dd043338d7494015661926b53
Crowdbotics <team@crowdbotics.com>
2020-11-17 16:52:33 +0000

    Initial commit.

---
+++ ios/fine_your_3some_22698Tests/fine_your_3some_22698Tests.m
@@ -0,0 +1,30 @@
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+#import <React/RCTLog.h>
+#import <React/RCTRootView.h>
+
+#define TIMEOUT_SECONDS 600
+#define TEXT_TO_LOOK_FOR @"Welcome to React"
+
+@interface fine_your_3some_22698 : XCTestCase
+
+@end
+
+@implementation fine_your_3some_22698
+
+- (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
+{
+  if (test(view)) {
+    return YES;
+  }
+  for (UIView *subview in [view subviews]) {
+    if ([self findSubviewInView:subview matching:test]) {
+      return YES;
+    }
+  }
+  return NO;
+}
+
+
+@end
